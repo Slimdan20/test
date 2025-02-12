668f6171e9c8d2f5c1f9ec6fc0aca05e07aa781c
Daniel Efe <daneytechwriter@gmail.com>
2025-02-12 08:44:18 -0800

    new commit

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,6 @@
 describe('Percy Visual Test', () => {
   it('should take a Percy snapshot', () => {
-    cy.visit('http://localhost:5173');
+    cy.visit('http://localhost:5173/');
     cy.percySnapshot('Homepage');
   });
 });
